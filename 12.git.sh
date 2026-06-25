3701ead17d25383c6aa2144eac5e20dc133e0c5f
aikdp <kdprasad028@gmail.com>
2026-06-25 17:07:33 +0530

    Shell Scripting Practice package installation

--- 12.git.sh
+++ 12.git.sh
@@ -15,17 +15,17 @@ if [ $USER -ne 0 ]
 fi
 
 dnf list installed nginx
-# if [ $? -ne 0 ]
-# then
-#     echo -e "ngnx is not installed, $Y goint to installed $N"
-#     dnf install nginx -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$R nginx instllation failed $N, please check"
-#         exit 1
-#     else 
-#         echo -e "NGINX installation completed $G SUCCESSFULLY $N"
-#     fi
-# else 
-#     echo -e "nginx already installed, $Y SKIPPING $N"
-# fi
+if [ $? -ne 0 ]
+then
+    echo -e "ngnx is not installed, $Y goint to installed $N"
+    dnf install nginx -y
+    if [ $? -ne 0 ]
+    then
+        echo "$R nginx instllation failed $N, please check"
+        exit 1
+    else 
+        echo -e "NGINX installation completed $G SUCCESSFULLY $N"
+    fi
+else 
+    echo -e "nginx already installed, $Y SKIPPING $N"
+fi
