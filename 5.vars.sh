61a916c26bf85cf73973f40602748b3aa6ccd8a5
aikdp <kdprasad028@gmail.com>
2026-06-25 16:14:09 +0530

    Shell Scripting Practice

--- 5.vars.sh
+++ 5.vars.sh
@@ -9,3 +9,11 @@ echo "Please enter Password::"
 read -s PASSWORD
 
 # echo "Password is: $PASSWORD"
+
+# echo "Pleasse enter you login id:"
+# read -s LOGIN
+# echo "ENterted login id is ::$LOGIN"
+
+# echo "Please enter your password::"
+# read -s PASS
+# echo "Entered Passsword is :: $PASS"
