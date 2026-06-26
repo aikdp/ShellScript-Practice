53e8144a8b6ee752117bac2b01887fd56670836e
aikdp <kdprasad028@gmail.com>
2026-06-26 14:58:13 +0530

    shell scritin with log backup

---
+++ 23.disk_uti.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+THRESHOLD=5
+
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}')
+    MOUNT_POINT=4(echo $line | grep xfs | awk -F '{print $NF}')
+    if [ $USAGE -ge $THRESHOLD ]
+    then
+        echo "$MOUNT_POINT is morethan $THRESHOLD, Currrent Value is:: $USAGE, PLEASE CHECK"
+    fi
+
+done <<< $DISK_USAGE
+
