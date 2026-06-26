83bbb6fdeb988d363d4daf9bf817965ebbc1a9bb
aikdp <kdprasad028@gmail.com>
2026-06-26 15:06:36 +0530

    shell scritin with log backup

--- 23.disk_uti.sh
+++ 23.disk_uti.sh
@@ -6,11 +6,12 @@ THRESHOLD=5
 
 while IFS= read -r line
 do
-    USAGE=$(echo "$line" | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    MOUNT_POINT=$(echo "$line" | grep xfs | awk -F '{print $NF}')
-    if [ "$USAGE" -ge "$THRESHOLD" ]
-    then
-        echo "$MOUNT_POINT is morethan $THRESHOLD, Currrent Value is:: $USAGE, PLEASE CHECK"
-    fi
+    echo "All lines are:: $line"
+    # USAGE=$(echo "$line" | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    # MOUNT_POINT=$(echo "$line" | grep xfs | awk -F '{print $NF}')
+    # if [ "$USAGE" -ge "$THRESHOLD" ]
+    # then
+    #     echo "$MOUNT_POINT is morethan $THRESHOLD, Currrent Value is:: $USAGE, PLEASE CHECK"
+    # fi
 
 done <<< "$DISK_USAGE"
