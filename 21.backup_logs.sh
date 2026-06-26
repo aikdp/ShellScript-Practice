ea51147ca69e8b43c3094e045d819b5eb4c50cfa
aikdp <kdprasad028@gmail.com>
2026-06-26 13:25:15 +0530

    shell scritin with log backup

--- 21.backup_logs.sh
+++ 21.backup_logs.sh
@@ -46,7 +46,7 @@ echo "Existing oldet]r than $DAYS days are:: $FILES_DEL "
 if [ ! -z "${FILES_DEL}" ]
 then
     echo -e "File older than 14days $Y are found $N, going to ZIP"
-    find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+    "$FILES_DEL" | zip "$ZIP_FILE" -@
     if [ -f $ZIP_FILE ]
     then
         echo -e "Files is Zipped $G successfully $N"
