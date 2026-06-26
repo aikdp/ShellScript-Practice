44badd04334eca2d1890d1a13fe57adb920fb5f8
aikdp <kdprasad028@gmail.com>
2026-06-26 11:07:32 +0530

    shell scritin with log deletion

---
+++ 19.log_delete.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+SOURCE_DIR=$1
+# DESTINATION_DIR=$2
+# DAYS=$(3: -14)
+
+if [ ! -d $SOURCE_DIR]
+then
+    echo "$SOURCE_DIR does not exists. please check"
+    exit 1
+fi
+
+# if [ ! -d $DESTINATION_DIR]
+# then
+#     echo "$DESTINATION_DIR does not exists. please check"
+#     exit 1
+# fi
+
+find $SOURCE_DIR -name "*.log" -mtime +14
