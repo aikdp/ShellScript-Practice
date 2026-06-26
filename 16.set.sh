33f0e6c1ac1fe3d44c4454160982e562bbe1470e
aikdp <kdprasad028@gmail.com>
2026-06-26 10:26:58 +0530

    shell scritin with log deletion

---
+++ 16.set.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set is uswed to exit when the script finds the error. it will stop immedidatley. "x" is for debugging
+set -ex 
+echo "Hello World"
+
+ehoo "Helo"
+
+echo "HI, checking SET commamd"
