735d4d916e42ca91085f2b773e28b935bdde8d8a
aikdp <kdprasad028@gmail.com>
2026-06-26 10:51:30 +0530

    shell scritin with log deletion

--- 18.trap.sh
+++ 18.trap.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-err_repo(){
-    echo "Error on line $1"
-}
+# err_repo(){
+#     echo "Error on line $1"
+# }
 
-trap 'err_repo "${LINENO}"' ERR
+# trap 'err_repo "${LINENO}"' ERR
+
+trap "echo 'Error finds at $1'" ERR
 
 echo "Hi, How are you"
 
